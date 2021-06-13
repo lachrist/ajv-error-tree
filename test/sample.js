b4014a943541c18cf7fafaad5927f47cb8acd0fa
Laurent Christophe <lachrist@vub.ac.be>
2021-06-13 17:17:06 +0200

    feat: tree-friendly property name for summarized tree

--- test/sample.js
+++ test/sample.js
@@ -1,5 +1,6 @@
 const Ajv = require("ajv");
-const { stringify } = require("yaml");
+const YAML = require("yaml");
+const { asTree } = require("treeify");
 const {
   structureAJVErrorArray,
   summarizeAJVErrorTree,
@@ -28,9 +29,14 @@ const data = { foo: "bar" };
 if (!validate(data)) {
   const tree1 = structureAJVErrorArray(validate.errors);
   const tree2 = summarizeAJVErrorTree(tree1);
-  console.log(stringify(tree2));
-  // message: /properties/foo/anyOf >> /foo must match a schema in anyOf
-  // children:
+  console.log(asTree(tree2, true));
+  // ├─ .: /properties/foo/anyOf >> /foo must match a schema in anyOf
+  // └─ +
+  //    ├─ 0: /properties/foo/anyOf/0/oneOf >> /foo must match exactly one schema in oneOf
+  //    └─ 1: /properties/foo/anyOf/1/type >> /foo must be number
+  console.log(YAML.stringify(tree2));
+  // .: /properties/foo/anyOf >> /foo must match a schema in anyOf
+  // +:
   //   - /properties/foo/anyOf/0/oneOf >> /foo must match exactly one schema in oneOf
   //   - /properties/foo/anyOf/1/type >> /foo must be number
 }
